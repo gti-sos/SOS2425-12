27b9233a30df5140d1a713f5e6654aa6c97e0910
Fer-28 <feragugar@alum.us.es>
2025-02-24 14:20:38 +0100

    index.html

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const cool = require("cool-ascii-faces")
+const express = require("express");
+const cool = require("cool-ascii-faces");
+const app = express();
+const PORT = process.env.PORT || 16078;
 
 app.get("/cool", (request, response) => {
     response.send(cool());
 });
+
+app.use("/", express.static("./public"));
