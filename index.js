4b19441c894d86e952d693174d69fc5131610ac7
Fer-28 <feragugar@alum.us.es>
2025-02-24 16:10:13 +0100

    Redirect /about

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 16078;
 
-app.use("/", express.static("./public"));
+app.use("/", express.static(__dirname));
 
 app.get("/cool", (request, response) => {
     response.send(cool());
+});
+
+app.get("/", (request, response) => {
+    response.redirect("/about");
 });
 
 app.listen(PORT, () => {
