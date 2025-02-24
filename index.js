17b9f018c8c27b44c7ae290111239068428668e3
Fer-28 <feragugar@alum.us.es>
2025-02-24 14:23:45 +0100

    Express implementation with cool

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 16078;
 
+app.use("/", express.static("./public"));
+
 app.get("/cool", (request, response) => {
     response.send(cool());
 });
 
-app.use("/", express.static("./public"));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+});
