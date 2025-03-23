2b7aa84af3fa08af2255863c35c8a7815fbe3924
Fer-28 <feragugar@alum.us.es>
2025-03-23 23:54:51 +0100

    FAG Final F05

--- index.js
+++ index.js
@@ -261,21 +261,23 @@ app.put(BASE_API + "/annual-retributions", (req, res) => {
 app.put(BASE_API + "/annual-retributions/:aacc", (req, res) => {
     let aacc = req.params.aacc;
     console.log(`New PUT to /annual-retributions/${aacc}`);
-
-    const index = annual_retributions.findIndex(x => x.aacc == aacc);
-    if (index >= 0){
-        let data = req.body;
-        annual_retributions[index] = {
-            ...annual_retributions[index],
-            ...data
-        };
-        res.status(200).json({message : "Datos actualizados"});
-        
+    if (aacc == req.body.aacc){
+        const index = annual_retributions.findIndex(x => x.aacc == aacc);
+        if (index >= 0){
+            let data = req.body;
+            annual_retributions[index] = {
+                ...annual_retributions[index],
+                ...data
+            };
+            res.status(200).json({message : "Datos actualizados"});
+            
+        }
+        else{   
+            return res.status(404).json({error: `No se encuentran datos de ${aacc}`});
+        }
+    } else {
+        return res.status(400).json({error: "No se puede modificar el id"});
     }
-    else{   
-        return res.status(404).json({error: `No se encuentran datos de ${aacc}`});
-    }
-
 });
 
 //DELETE
