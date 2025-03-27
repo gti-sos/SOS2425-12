c3fbc6af564d16cd5eddac71b51e45c8581693c6
Fer-28 <feragugar@alum.us.es>
2025-03-27 11:13:08 +0100

    Lo rompí

---
+++ index.js
@@ -0,0 +1,21 @@
+//const express = require("express");
+import express from "express";
+import { loadBackendGOS } from "./src/back/index-GOS.js";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 16078;
+
+
+app.use(express.json());
+app.use("/",express.static("./public"));
+
+loadBackendGOS(app);
+
+app.get("/about", (request, response) => {
+    response.sendFile(path.resolve("./about/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+});
