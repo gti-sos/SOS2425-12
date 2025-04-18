e55987c6c5c2fb9d4a7dc6a92a9d5921290ecbfc
Fer-28 <feragugar@alum.us.es>
2025-04-18 15:43:15 +0200

    update

--- index.js
+++ index.js
@@ -2,20 +2,14 @@ import express from "express";
 import { loadBackendGOS } from "./src/back/index-GOS.js";
 import { loadBackendFAG } from "./src/back/index-FAG.js";
 import { loadBackendCRR } from "./src/back/index-CRR.js";
-
-import cors from "cors";
 import { handler } from "./src/front/build/handler.js";
-import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 16078;
 
 
 app.use(express.json());
-app.use("/",express.static("./public"));
-app.use(cors());
-// app.use(handler);
-
+// app.use("/",express.static("./public"));
 
 loadBackendGOS(app);
 
@@ -23,9 +17,11 @@ loadBackendFAG(app);
 
 loadBackendCRR(app);
 
-app.get("/about", (request, response) => {
-    response.sendFile(path.resolve("./about/index.html"));
-});
+// app.get("/about", (request, response) => {
+//     response.sendFile(path.resolve("./about/index.html"));
+// });
+
+app.use(handler)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}!`);
