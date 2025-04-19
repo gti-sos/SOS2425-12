cdfd3d4d5c57896fd7b643a078ce7ddb5bfd8771
Fer-28 <feragugar@alum.us.es>
2025-04-19 14:06:22 +0200

    sveltestrap annual-retributions

--- src/front/src/routes/annual-retributions/+page.svelte
+++ src/front/src/routes/annual-retributions/+page.svelte
@@ -1,8 +1,14 @@
 <script>
 //  @ts-nocheck
   import { onMount } from "svelte";
+  import { Button, Table } from "@sveltestrap/sveltestrap";
+  import { dev } from "$app/environment";
 
-  let API = "http://localhost:16078/api/v1/annual-retributions";
+  let DEVEL_HOST = "http://localhost:16078";
+  let API = "/api/v1/annual-retributions";
+  if (dev) {
+    API = DEVEL_HOST + API;
+  }
   let annual_retributions = [];
   let result = "";
   let resultStatus = "";
@@ -26,41 +32,66 @@
   });
 </script>
 
-<table>
-  <!-- year: 2018,
-  technology: "Cogeneración",
-  subsidized_energy: 25935.3492,
-  total_compensation: 2713723.05,
-  investment_compensation: 81485.92,
-  operation_compensation: 1127022.62,
-  specific_compensation: 1208508.54,
-  aacc: "Asturias, Principado de" -->
-  <thead>
-    <tr>
-      <th>Year</th>
-      <th>Technology</th>
-      <th>Subsidized Energy</th>
-      <th>Total Compensation</th>
-      <th>Investment Compensation</th>
-      <th>Operation Compensation</th>
-      <th>Specific Compensation</th>
-      <th>AACC</th>
-      <th>Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each annual_retributions as retribution}
+<style>
+  div#body {
+    font-family: Arial;
+    border: #cecdcd solid 1px;
+    margin: 2%;
+    padding: 0.5%;
+  }
+  /* table {
+    width: 100%;
+    border-collapse: collapse;
+  } */
+  th, td {
+    border: 1px solid #cecdcd;
+    padding: 8px;
+  }
+  th {
+    background-color: #aca5a5c2;
+    text-align: left;
+  }
+</style>
+
+<div id="body">
+  <Table>
+    <!-- year: 2018,
+    technology: "Cogeneración",
+    subsidized_energy: 25935.3492,
+    total_compensation: 2713723.05,
+    investment_compensation: 81485.92,
+    operation_compensation: 1127022.62,
+    specific_compensation: 1208508.54,
+    aacc: "Asturias, Principado de" -->
+    <thead>
       <tr>
-        <td>{retribution.year}</td>
-        <td>{retribution.technology}</td>
-        <td>{retribution.subsidized_energy}</td>
-        <td>{retribution.total_compensation}</td>
-        <td>{retribution.investment_compensation}</td>
-        <td>{retribution.operation_compensation}</td>
-        <td>{retribution.specific_compensation}</td>
-        <td>{retribution.aacc}</td>
-        <td><button class="btn btn-info">Actualizar</button>
-        <td><button class="btn btn-danger">Borrar</button></td>
+        <th>Year</th>
+        <th>Technology</th>
+        <th>Subsidized Energy</th>
+        <th>Total Compensation</th>
+        <th>Investment Compensation</th>
+        <th>Operation Compensation</th>
+        <th>Specific Compensation</th>
+        <th>AACC</th>
+        <th>Actions</th>
       </tr>
-    {/each}
-</table>
+    </thead>
+    <tbody>
+      {#each annual_retributions as retribution}
+        <tr>
+          <td>{retribution.year}</td>
+          <td>{retribution.technology}</td>
+          <td>{retribution.subsidized_energy}</td>
+          <td>{retribution.total_compensation}</td>
+          <td>{retribution.investment_compensation}</td>
+          <td>{retribution.operation_compensation}</td>
+          <td>{retribution.specific_compensation}</td>
+          <td>{retribution.aacc}</td>
+          <td>
+          <Button color="info">Actualizar</Button>
+          <Button color="danger">Borrar</Button>
+          </td>
+        </tr>
+      {/each}
+  </Table>
+</div>
