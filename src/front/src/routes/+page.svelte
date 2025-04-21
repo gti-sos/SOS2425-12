9ac57d5a693b72ba6281ca40b95a7441448aa025
Fer-28 <feragugar@alum.us.es>
2025-04-21 13:51:44 +0200

    :sparkles: homePage

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -1,11 +1,46 @@
 <script>
     console.log("Page loaded");
 </script>
+<style>
+    div#body{
+        margin: 1%;
+    }
+</style>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SOS2425-12</title>
 </head>
-<body>
+<div id=body>
     <h1>Bienvenido a la API del grupo 12 de SOS</h1>
-</body>
+
+    <h2>Aplicaciones del equipo</h2>
+    <ul>
+        <li><a href="/annual_retributions">Retribuciones anuales</a> (Fernando Aguirre García)</li>
+        <li><a href="/annual_evolutoins">Evoluciones anuales </a> (Clara Reina Romero)</li>
+        <li><a href="/annual_consumptions">Consumos anuales</a> (Gonzalo Osuna Salas)</li>
+    </ul>
+
+
+    <h2>Datos analizados y APIs</h2>
+    <ul>
+        <li><a href="/api/v1/annual_retributions">API Retribuciones anuales</a> (Fernando Aguirre García)</li>
+        <li><a href="/api/v1/annual_evolutoins">API Evoluciones anuales </a> (Clara Reina Romero)</li>
+        <li><a href="/api/v1/annual_consumptions">API Consumos anuales</a> (Gonzalo Osuna Salas)</li>
+    </ul>
+
+    <h2>Documentación de las APIs</h2>
+    <ul>
+        <li><a href="/api/v1/annual_retributions/docs">Documentación Retribuciones anuales</a> (Fernando Aguirre García)</li>
+        <li><a href="/api/v1/annual_evolutoins/docs">Documentación Evoluciones anuales </a> (Clara Reina Romero)</li>
+        <li><a href="/api/v1/annual_consumptions/docs">Documentación Consumos anuales</a> (Gonzalo Osuna Salas)</li>
+    </ul>
+    <h2><a href="https://github.com/gti-sos/SOS2425-12">Repositorio del proyecto</a> </h2>
+
+    <h2> Integrantes del equipo </h2>
+    <ul>
+        <li><a href="https://github.com/Fer-28">Fernando Aguirre García </a>– Fuente de datos: annual_retributions</li>
+        <li><a href="https://github.com/clarareina">Clara Reina Romero</a> – Fuente de datos: annual_evolutoins</li>
+        <li><a href="https://github.com/gxnzalx55">Gonzalo Osuna Salas</a> – Fuente de datos: annual_consumptions</li>
+    </ul>
+</div>
